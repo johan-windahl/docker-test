548dcb5944d4f10afdad3905216b71b94651cee0
Johan Windahl <johan.windahl@evokegaming.com>
2017-08-29 21:19:02 +0200

    .

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const http = require('http');
 
 const hostname = '0.0.0.0'; //IP adress exposed outside docker
-const port = 3888;
+//const port = 3888;
+const port = process.env.PORT;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
