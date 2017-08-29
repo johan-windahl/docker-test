208535da337be652dedb4c53aca17d3cade8ceab
Johan Windahl <johan.windahl@evokegaming.com>
2017-08-29 15:37:51 +0200

    localhost

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const http = require('http');
 
-const hostname = '0.0.0.0'; //IP adress exposed outside docker
+//const hostname = '0.0.0.0'; //IP adress exposed outside docker
+const hostname = '127.0.0.1'; //IP adress exposed outside docker
 const port = 3888;
 
 const server = http.createServer((req, res) => {
